19f533f480ee95259c08992400416eaf87f666dd
idk-v1 <benhamilton.jpg@gmail.com>
2025-03-10 21:41:09 -0500

    Deletes ants when on border

--- Ants.c
+++ Ants.c
@@ -16,12 +16,14 @@ typedef struct Ant
 {
 	vec2f pts[ANT_SEGS];
 	int32_t dir;
+	bool deleted;
 } Ant;
 
 typedef struct Colony
 {
 	vec2f pt;
 	Ant* ants;
+	uint64_t count;
 	uint64_t numAnts;
 	uint64_t maxAnts;
 } Colony;
@@ -32,40 +34,80 @@ static Colony createColony(sft_window* win)
 	colony.pt.x = win->width / 2;
 	colony.pt.y = win->height / 2;
 
+	colony.count = 0;
 	colony.numAnts = 0;
 	colony.maxAnts = 1;
 	colony.ants = malloc(colony.maxAnts * sizeof(Ant));
 	return colony;
 }
 
+static void deleteColony(Colony* colony)
+{
+	free(colony->ants);
+	colony->ants = NULL;
+	colony->count = 0;
+	colony->numAnts = 0;
+	colony->maxAnts = 0;
+}
+
+static void deleteAnt(Colony* colony, uint64_t index)
+{
+	if (index < colony->numAnts)
+	{
+		colony->ants[index].deleted = true;
+		colony->count--;
+	}
+}
+
 static void addAnt(Colony* colony)
 {
-	// if array is filled, resize
-	if (colony->numAnts + 1 >= colony->maxAnts)
-	{
-		Ant* temp = realloc(colony->ants, colony->maxAnts * 2 * sizeof(Ant));
-		if (temp)
+	// search for deleted ant in array to replace first
+	bool replace = false;
+
+	uint64_t i = 0;
+	for (; i < colony->numAnts; i++)
+		if (colony->ants[i].deleted)
 		{
-			colony->maxAnts *= 2;
-			colony->ants = temp;
+			replace = true;
+			break;
 		}
-		else
-			return; // no more memory, keep existing ants
+
+	// if need to add new ant instead of replacing
+	// if array is filled, resize
+	if (!replace)
+	{
+		if (colony->numAnts + 1 >= colony->maxAnts)
+		{
+			Ant* temp = realloc(colony->ants, colony->maxAnts * 2 * sizeof(Ant));
+			if (temp)
+			{
+				colony->maxAnts *= 2;
+				colony->ants = temp;
+			}
+			else
+				return; // no more memory, keep existing ants
+		}
+
+		i = colony->numAnts;
+		colony->numAnts++;
 	}
 
 	// spawn ant at the colony (middle of screen)
 	// ant has random direction
-	for (uint64_t i = 0; i < ANT_SEGS; i++)
-		colony->ants[colony->numAnts].pts[i] = colony->pt;
-	colony->ants[colony->numAnts].dir = rand() % (360 / 5) * 5;
-
-	colony->numAnts++;
+	for (uint64_t s = 0; s < ANT_SEGS; s++)
+		colony->ants[i].pts[s] = colony->pt;
+	colony->ants[i].dir = rand() % (360 / 5) * 5;
+	colony->ants[i].deleted = false;
+	colony->count++;
 }
 
 static void updateAnts(sft_window* win, Colony* colony)
 {
 	for (uint64_t i = 0; i < colony->numAnts; i++)
 	{
+		if (colony->ants[i].deleted)
+			continue;
+
 		// move the last segment to the next segment,
 		// then the next segment to the next next segment, repeat
 		// the head is controlled by the direction
@@ -94,6 +136,7 @@ static void updateAnts(sft_window* win, Colony* colony)
 			colony->ants[i].pts[0].y <= 0)
 		{
 			// TODO steal pixel
+			deleteAnt(colony, i);
 		}
 	}
 }
@@ -103,9 +146,15 @@ static void drawAnts(sft_window* win, Colony* colony)
 	static int flash = 0;
 	flash++;
 
-	// debug (or just bug) thingy to make ants easier to see
+	// draw colony
+	sft_window_drawRect(win, colony->pt.x - 2, colony->pt.y - 2, 5, 5, 0x7F7F3F00);
 
+	// debug (or just bug) thingy to make ants easier to see
 	for (uint64_t i = 0; i < colony->numAnts; i++)
+	{
+		if (colony->ants[i].deleted)
+			continue;
+
 		for (uint64_t s = 0; s < ANT_SEGS; s++)
 		{
 			if (sft_input_keyState(sft_key_Capslock))
@@ -115,6 +164,7 @@ static void drawAnts(sft_window* win, Colony* colony)
 				sft_window_drawRect(win, colony->ants[i].pts[s].x,
 					colony->ants[i].pts[s].y, 1, 1, 0xFF000000);
 		}
+	}
 }
 
 int main()
@@ -149,6 +199,8 @@ int main()
 
 		sft_sleep(10);
 	}
+
+	deleteColony(&colony);
 	sft_window_close(win);
 
 	sft_shutdown();
